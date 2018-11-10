5cfe9406a814ad6fcfb2efbdb06dc0c9a6b6fcec
deleted1313 <ultiwd@gmail.com>
2018-11-11 02:45:12 +0300

    initial

--- src/store.js
+++ src/store.js
@@ -1,16 +1,57 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    todos: [],
+    currentTodoId: null
   },
   mutations: {
+    async loadTodos(state) {
+      const response = await axios.get("http://localhost:3000/users");
+      state.todos = response.data;
+    },
+    createTodo(state, text, price) {
+      const todo = {
+        text,
+        price
+      };
+      todo.id = state.todos.length + 1;
+      state.todos.push(todo);
+      axios.post("http://localhost:3000/users", todo);
 
+      //отправка на бэк
+    },
+    updateTodo(state, todo) {
+      state.todos.map(oldTodo => {
+        if (oldTodo.id === todo.id) {
+          return todo;
+        }
+        return oldTodo;
+      });
+      axios.put(`http://localhost:3000/users/${todo.id}`, todo);
+    },
+    deleteTodo(state, todo) {
+      state.todos.splice([todo.id - 1], 1);
+      axios.delete(`http://localhost:3000/users/${todo.id}`, todo);
+
+      // отправка на бэк
+    }
   },
   actions: {
-
+    createTodo(context, todo) {
+      context.commit("createTodo", todo);
+    },
+    deleteTodo(context, todo) {
+      context.commit("deleteTodo", todo);
+    },
+    updateTodo(context, todo) {
+      context.commit("updateTodo", todo);
+    },
+    loadTodos(context) {
+      context.commit("loadTodos");
+    }
   }
-})
+});
